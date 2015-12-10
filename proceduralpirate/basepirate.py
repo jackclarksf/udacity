46efd2b53a3ce26c05e9b0adbefd70c951be6025
mappingbabel <iamjackc@gmail.com>
2015-12-09 21:18:39 -0800

    various things

--- proceduralpirate/basepirate.py
+++ proceduralpirate/basepirate.py
@@ -24,7 +24,32 @@ class Map_Procedural:
         self.base_map = [["$" for i in range(size)] for i in range(size)]
         self.base_terrain = []
         self.water = []
-        print("OK, your base map is: {}".format(self.base_map))
+        #print("OK, your base map is: {}".format(self.base_map))
+        self.water_generation()
+
+    def water_generation(self):
+        water_level = input("Please specify how wet you'd like it, on a scale of one to three: ")
+        sizes = [0, 1, 2, 3]
+        while int(water_level) not in sizes:
+            print("Your selection of {} is invalid as is not in {} ".format(water_level, sizes))
+            water_level = input("Try again (1 to 3), please: ")
+        else:
+            water_math = (len(self.base_map[0])*len(self.base_map[0]))
+            print("Our water math is {} divided by water_level input of {}".format(water_math, water_level))
+            squares_to_fill = int(water_math) / int(water_level)
+            print("We need to fill {} squares".format(squares_to_fill))
+            #PROBABLY NEED TO SORT OUT THE FLOAT THING HERE
+            #INITIALIZE THE SCREEN_DRAW FUNCTION HERE
+            self.draw_new_entities("W", squares_to_fill)
+
+    def draw_new_entities(self, entity_to_draw, entity_count):
+        i = 0
+        while i < entity_count:
+            for i in self.base_map:
+                self.base_map[i][i].replace("$", entity_to_draw)
+                i += 1
+                #NEED TO FIX THIS LIST NAVIGATION, PROBABLY
+
 
     def display_map(self):
         for i in self.base_map:
@@ -35,8 +60,8 @@ class Map_Procedural:
         i = 0
         dog = (len(self.base_map))
         while i < (dog-1):
-            print(i)
-            print(self.base_map[i])
+            #print(i)
+            #print(self.base_map[i])
             i += 1
             linked = ', '.join(self.base_map[i])
             linked_clean = linked.replace(",", " ")
